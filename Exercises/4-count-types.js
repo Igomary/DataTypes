4e151e841d28bd3d30f6a6e0d525c9e4680f92a3
Maria Presnukhina <maria@YEBYULs-MacBook-Pro.local>
2019-11-01 10:42:53 +0200

    2-4 added

--- Exercises/4-count-types.js
+++ Exercises/4-count-types.js
@@ -1,5 +1,13 @@
 'use strict';
 
-const countTypesInArray = null;
+
+const countTypesInArray = array => {
+  const collection = {};
+  for (const el of array) {
+    const memType = typeof(el);
+    collection[memType] ? ++collection[(memType)] : collection[(memType)] = 1;
+  }
+  return collection;
+};
 
 module.exports = { countTypesInArray };
